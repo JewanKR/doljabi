b237ef909f6079fb7458ec8f3eeeda02df63948e
unknown <ssoyul675@gmail.com>
2025-11-14 18:12:01 +0900

    251114

--- doljabi_engine/src/soyul/session_service.rs
+++ doljabi_engine/src/soyul/session_service.rs
@@ -1,16 +1,16 @@
 // src/soyul/session_service.rs
 
-use crate::soyul::session_store::{SessionStore};
+use crate::soyul::session_store::{SessionStore, UserId};
 
 /// 세션 추가: (session_key -> user_id)
-pub async fn insert_session(store: &SessionStore, session_key: String, user_id: u64) {
+pub async fn insert_session(store: &SessionStore, session_key: String, user_id: UserId) {
     let mut map = store
         .write();
     map.await.insert(session_key, user_id);
 }
 
 /// 세션 조회: 세션키로 user_id 얻기
-pub async fn get_user_id_by_session(store: &SessionStore, session_key: &str) -> Option<u64> {
+pub async fn get_user_id_by_session(store: &SessionStore, session_key: &str) -> Option<UserId> {
     let map = store
         .read();
     map.await.get(session_key).copied()
