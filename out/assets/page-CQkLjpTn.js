808c4de6c50123957131d17dfaa70c502f88e421
JewanKR <jyjen0015@gnu.ac.kr>
2025-12-01 22:11:06 +0900

    update gitignore

--- out/assets/page-CQkLjpTn.js
+++
@@ -1,2 +0,0 @@
-import{u as t,r as e}from"./index-DJxcKVzD.js";function r(){const a=t();return e.useEffect(()=>{a("/baduk/waiting-room")},[a]),null}export{r as default};
-//# sourceMappingURL=page-CQkLjpTn.js.map
